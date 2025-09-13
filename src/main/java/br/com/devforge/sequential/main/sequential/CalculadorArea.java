9ef0712267c6da3ca116bf53aec18dd886b5cee8
Lucas Brenner Ferreira <lucasbref82@gmail.com>
2025-09-13 00:11:49 -0300

    Criação da classe main

---
+++ src/main/java/br/com/devforge/sequential/main/sequential/CalculadorArea.java
@@ -0,0 +1,27 @@
+package br.com.devforge.sequential.main.sequential;
+
+import br.com.devforge.sequential.factory.FormaGeometricaFactory;
+import br.com.devforge.sequential.itf.FormaGeometrica;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculadorArea {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        System.out.println("******** Bem vindo ao calculador de área ********\n");
+        System.out.println("Escolha uma das opções: ");
+        System.out.println("1 = Circulo");
+        System.out.println("2 = Retangulo");
+        opcao = scanner.nextInt();
+        try {
+            FormaGeometrica formaGeometrica = FormaGeometricaFactory.criaFormaGeometrica(opcao, scanner);
+            System.out.printf("Área = %.4f", formaGeometrica.calcularArea());
+        } catch (IllegalArgumentException e) {
+            System.err.print(e.getMessage());
+        }
+        scanner.close();
+    }
+}
